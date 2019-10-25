9dbc7c124683505b39ba65add71abdf3af09e591
Elise Schofield <eas0020@uah.edu>
2019-10-25 10:10:36 -0500

    I added a basic menu for theme selection and found a way to keep GameGUI from continuing while the menu is open.

---
+++ src/GameGUI.java
@@ -0,0 +1,20 @@
+
+
+public class GameGUI {
+    public static void main(String[] args){
+        ThemeSelectionGUI test = new ThemeSelectionGUI();
+        test.displayThemeMenu();
+
+        //idk if this is a valid way to do this, but this chunk of code is meant to keep the program from continuing
+        // while the frame is active
+        int hold = 0;
+        do {
+            if (test.isThemeMenuActive()) {
+            } else {
+                hold = 1;
+            }
+        } while (hold != 1);
+
+        System.out.println(test.getThemeChoice());
+    }
+}
