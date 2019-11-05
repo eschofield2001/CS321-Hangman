81a8d397d79cf47146d42d54f2085282851eee8a
Elise Schofield <eas0020@uah.edu>
2019-11-05 17:16:09 -0600

    Here's the updated ThemeSelectionGUI

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -5,16 +5,8 @@ public class GameGUI {
         ThemeSelectionGUI test = new ThemeSelectionGUI();
         test.displayThemeMenu();
 
-        //idk if this is a valid way to do this, but this chunk of code is meant to keep the program from continuing
-        // while the frame is active
-        int hold = 0;
-        do {
-            if (test.isThemeMenuActive()) {
-            } else {
-                hold = 1;
-            }
-        } while (hold != 1);
-
-        System.out.println(test.getThemeChoice());
+        System.out.println("The user chose theme: " + test.getThemeChoice());
     }
 }
+
+
--- src/ThemeSelectionGUI.java
+++ src/ThemeSelectionGUI.java
@@ -1,84 +1,29 @@
-import java.awt.FlowLayout;
-import java.awt.event.*;
 import javax.swing.*;
 
 /**
  *This class is responsible for displaying a menu that allows the user to choose which theme they would like to play.
  */
 public class ThemeSelectionGUI {
-    //1-classic, 2-multiplayer, 3-under the sea, 4-halloween
-    private int themeChoice = -1;
-    private JFrame themeMenu = new JFrame();
+    //0-classic, 1-multiplayer, 2-under the sea, 3-halloween
+    private int themeChoice;
+
+    /**
+     * Constructor for ThemeSelectionGUI that initializes themeChoice to -1
+     */
+    ThemeSelectionGUI(){
+        themeChoice = -1;
+    }
 
     /**
      * This method is responsible for displaying the menu and storing the user's selection.
      */
     public void displayThemeMenu(){
-        //JFrame themeMenu = new JFrame();
+        String[] options = {"Classic", "Multiplayer", "Under the Sea", "Halloween"};
+        JComboBox themes = new JComboBox(options);
 
-        //Text field set up
-        final int FIELD_WIDTH = 30;
-        final JTextField textField = new JTextField(FIELD_WIDTH);
-        textField.setText("Choose which theme you would like to play.");
+        JOptionPane.showMessageDialog(null, themes, "Select Theme", JOptionPane.QUESTION_MESSAGE);
 
-        //Classic button set up
-        JButton classicButton = new JButton("Classic");
-        classicButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                themeChoice = 1;
-                themeMenu.dispose();
-                //System.out.println("closed"); for testing
-            }
-        });
-
-        //Multiplayer button set up
-        JButton multiButton = new JButton("Multiplayer");
-        multiButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event){
-                themeChoice = 2;
-                themeMenu.dispose();
-            }
-        });
-
-        //Under the sea button set up
-        JButton seaButton = new JButton("Under the Sea");
-        seaButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event){
-                themeChoice = 3;
-                themeMenu.dispose();
-            }
-        });
-
-        //Halloween button set up
-        JButton halloweenButton = new JButton("Halloween");
-        halloweenButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event){
-                themeChoice = 4;
-                themeMenu.dispose();
-            }
-        });
-
-        themeMenu.setLayout(new FlowLayout());
-
-        themeMenu.add(classicButton);
-        themeMenu.add(multiButton);
-        themeMenu.add(seaButton);
-        themeMenu.add(halloweenButton);
-        themeMenu.add(textField);
-
-        themeMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        themeMenu.pack();
-        themeMenu.setLocationRelativeTo(null);
-        themeMenu.setVisible(true);
-    }
-
-    /**
-     * The purpose of this method is to return whether or not themeMenu is still active.
-     * This can be used to prevent the main program from continuing its execution while themeMenu is still open.
-     * @return boolean, true if themeMenu is active, false if it isn't
-     */
-    public boolean isThemeMenuActive(){
-        return themeMenu.isActive();
+        themeChoice = themes.getSelectedIndex();
     }
 
     /**
@@ -90,3 +35,4 @@ public class ThemeSelectionGUI {
     }
 
 }
+
